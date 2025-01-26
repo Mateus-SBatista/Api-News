b49f13e34b49a9a0135ba1cd43955ece08753945
Mateus Batista <mateusbatista.mb14@gmail.com>
2025-01-26 14:26:16 -0300

    ajustes nas urls e na view

--- api_noticias/urls.py
+++ api_noticias/urls.py
@@ -3,10 +3,10 @@ from .views import NoticiaListCreateView, NoticiaDetailView, MemoriaNoticiaListC
 
 urlpatterns = [
     # Endpoints para o banco SQLite
-    path('noticias/', NoticiaListCreateView.as_view(), name='noticia-list-sqlite'),
-    path('noticias/<int:pk>/', NoticiaDetailView.as_view(), name='noticia-detail-sqlite'),
+    path('api_noticias/', NoticiaListCreateView.as_view(), name='noticia-list-sqlite'),
+    path('api_noticias/<int:pk>/', NoticiaDetailView.as_view(), name='noticia-detail-sqlite'),
 
     # Endpoints para o banco de memória
-    path('memoria/noticias/', MemoriaNoticiaListCreateView.as_view(), name='noticia-list-memoria'),
-    path('memoria/noticias/<str:pk>/', MemoriaNoticiaDetailView.as_view(), name='noticia-detail-memoria'),
+    path('memoria/api_noticias/', MemoriaNoticiaListCreateView.as_view(), name='noticia-list-memoria'),
+    path('memoria/api_noticias/<str:pk>/', MemoriaNoticiaDetailView.as_view(), name='noticia-detail-memoria'),
 ]
--- setup/urls.py
+++ setup/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('noticias.urls')),
+    path('api/', include('api_noticias.urls')),
 ]
