4ba80f37a404bb0b9628d9cf6bf46650bf345118
Mateus Batista <mateusbatista.mb14@gmail.com>
2025-01-26 13:51:40 -0300

    criação das migrações e banco de dados

--- api_noticias/models.py
+++ api_noticias/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Noticia(models.Model):
+    titulo = models.CharField(max_length=255)
+    conteudo = models.TextField()
+    autor = models.CharField(max_length=255)
+    data_publicacao = models.DateTimeField(auto_now_add=True)
+    ativo = models.BooleanField(default=True)  # Indica se a notícia está ativa
+
+    def __str__(self):
+        return self.titulo
